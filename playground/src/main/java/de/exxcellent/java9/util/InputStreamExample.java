fece81a7acf5b9d1144d710a47bfe78a5694c3c7
YouDiSN <839494773@qq.com>
2017-09-27 23:04:33 +0800

    refactor(InputStreamExample): refactor the code style

--- playground/src/main/java/de/exxcellent/java9/util/InputStreamExample.java
+++ playground/src/main/java/de/exxcellent/java9/util/InputStreamExample.java
@@ -14,17 +14,24 @@ public class InputStreamExample {
      * Stream utilities.
      */
     public static void main(String[] args) throws IOException {
+        randomByte();
+        readJson();
+    }
+
+    private static void readJson() throws IOException {
+        // All bytes from an InputStream at once
+        byte[] fileByte = Files.readAllBytes(FileSystems.getDefault().getPath(JSON_PATH));
+        String json = new String(fileByte, "UTF-8");
+        System.out.println(json);
+    }
+
+    private static void randomByte() throws IOException {
         // 128 random bytes
         byte[] buf = new byte[128];
         new Random().nextBytes(buf);
         byte[] result = new ByteArrayInputStream(buf).readAllBytes();
 //         Directly redirect an InputStream to an OutputStream
         new ByteArrayInputStream(result).transferTo(System.out);
-
-        // All bytes from an InputStream at once
-        byte[] fileByte = Files.readAllBytes(FileSystems.getDefault().getPath(JSON_PATH));
-        String json = new String(fileByte, "UTF-8");
-        System.out.println(json);
     }
 
 }
